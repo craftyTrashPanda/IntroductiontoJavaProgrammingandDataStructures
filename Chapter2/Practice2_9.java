06b9be37fef2a20abf9f09472f8018d9b5fa5823
craftyTrashPanda <pegramjm@gmail.com>
2024-09-09 14:42:50 -0400

    Update Practice2_9.java
    Forgot to comment out a few lines

--- Chapter2/Practice2_9.java
+++ Chapter2/Practice2_9.java
@@ -15,11 +15,11 @@ public class ChTwoProblems {
 		// TODO Auto-generated method stub
 		
 		Scanner input = new Scanner(System.in);
-		
-		 2.9
-		 Calculate acceleration as defined by the below equation
-		 a = v0 - v1 / t
-		 acceleration equals starting velocity in meters per second minus ending velocity meters per second divided by time span in seconds
+//		
+//		 2.9
+//		 Calculate acceleration as defined by the below equation
+//		 a = v0 - v1 / t
+//		 acceleration equals starting velocity in meters per second minus ending velocity meters per second divided by time span in seconds
 		System.out.println("Enter starting velocity, ending velocity, and time span in seconds to calculate velocity: ");
 		double starting = input.nextDouble();
 		double ending = input.nextDouble();
